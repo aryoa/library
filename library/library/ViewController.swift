86d9f833b0ae579b11ddb96690d3777fdef332b4
HIROKI YAMAMOTO <hiroki.yamamoto@jrits.ricoh.co.jp>
2015-08-22 17:15:47 +0900

    no message

---
+++ library/library/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  library
+//
+//  Created by HIROKI YAMAMOTO on 2015/08/22.
+//  Copyright (c) 2015年 arao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
